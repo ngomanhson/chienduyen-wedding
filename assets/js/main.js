429a6944746893103d776b9c475c33ff5c348469
ngomanhson <ngomanhson2004txpt@gmail.com>
2026-03-06 14:31:03 +0700

    fix: responsive mobile & update icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,7 +115,10 @@ function toggleMusic() {
                     '<path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>';
                 btn.style.borderColor = "rgba(201,168,76,.7)";
             })
-            .catch(() => alert("Nhấn lại để bật nhạc 🎵"));
+            .catch((err) => {
+                console.log("Toggle Music error: ", err);
+                alert("Nhấn lại để bật nhạc 🎵");
+            });
     }
 }
 
